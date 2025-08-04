48c128bada10457a9fe743b52cd4c44fefcda928
SaraTohidian <sara.tohidian@DESKTOP-CMIQN01>
2025-08-04 16:45:32 +0330

    AggregateRoot

--- ExampleProjectDDD.Domain/Entities/CourseManagment/Course.cs
+++ ExampleProjectDDD.Domain/Entities/CourseManagment/Course.cs
@@ -1,16 +1,20 @@
 ﻿using ExampleProjectDDD.Domain.Primitives;
 using ExampleProjectDDD.Domain.ValueObject;
+using ExampleProjectDDD.Shared.Abstraction.Domain;
 
 namespace ExampleProjectDDD.Domain.Entities.CourseManagment
 {
-    public class Course:BaseEntity
+    //becuse this class is Aggregrate i impliment from AggregateRoot<T>
+    public class Course : AggregateRoot<BaseId>
     {
-        public Course(BaseId id):base(id) { }
+        public Course() { }
 
-        internal Course(BaseId id,Description description, Title title, Price price, BaseId instructor_ID):base(id)
+        internal Course(BaseId id, Description description, Title title, Price price, BaseId instructor_ID)
         {
+            //ID is AggregateRoot's property
+            ID = id;
             _description = description;
-            _title = title; 
+            _title = title;
             _price = price;
             _instructor_ID = instructor_ID;
         }
---
+++ ExampleProjectDDD.Shared.Abstraction/Domain/AggregateRoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProjectDDD.Shared.Abstraction.Domain
+{
+    public abstract class AggregateRoot<T>
+    {
+        public T ID { get; protected set; }
+
+        //for do you want know more you can read => Event Sourcing
+        public int Version { get; protected set; }
+        private bool _isIncremented;
+        protected void IncrementVersion()
+        {
+            if (_isIncremented) return;
+            Version++;
+            _isIncremented = true;
+        }
+    }
+}
